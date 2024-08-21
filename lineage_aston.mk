e2a86f7bbe0202dc132330bd3c75a15bbac1abbd
Jorge <jorgeim97@gmail.com>
2024-08-21 12:51:47 +0200

    Actualizar lineage_aston.mk

--- lineage_aston.mk
+++ lineage_aston.mk
@@ -9,25 +9,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from aston device
-$(call inherit-product, device/oneplus/aston/device.mk)
+$(call inherit-product, device/oneplus/xigua/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_aston
-PRODUCT_DEVICE := aston
+PRODUCT_NAME := lineage_xigua
+PRODUCT_DEVICE := xigua
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2585
+PRODUCT_MODEL := PJA110
 
 PRODUCT_SYSTEM_NAME := $(PRODUCT_MODEL)
-PRODUCT_SYSTEM_DEVICE := OP5D35L1
+PRODUCT_SYSTEM_DEVICE := PJA110
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="CPH2585IN-user 14 TP1A.220905.001 U.R4T3.19135c3-17dcf-a29fb release-keys" \
+    PRIVATE_BUILD_DESC="PJA110-user 14 TP1A.220905.001 U.R4T3.19135c3-17dcf-a29fb release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/CPH2585IN/OP5D35L1:14/TP1A.220905.001/U.R4T3.19135c3-17dcf-a29fb:user/release-keys
+BUILD_FINGERPRINT := OnePlus/PJA110/PJA110:14/TP1A.220905.001/U.R4T3.19135c3-17dcf-a29fb:user/release-keys
